967ef899fb1269b78f42fdc74e2ba9a25eb8800c
JinxYi <jingyi0939@gmail.com>
2025-11-30 23:10:43 +0800

    feat: added python notebook for prediction model

--- config.py
+++ config.py
@@ -7,3 +7,5 @@ BENCHMARK_DIR = "reports/benchmarks"
 # Reproducability
 STREAM_SHUFFLE_BUFFER_SIZE = 5000 # WARNING: changing this will change the test set across runs; not recommended to change 
 SEED = 42
+SEED_2 = 101
+SEED_3 = 211
